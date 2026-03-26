36ce147e75bc33741ff41182188dd02c0564692c
Aaron Chan <aarchan108@gmail.com>
2026-03-26 11:49:12 -0400

    Try saga solver for minirocket

--- train_mini_rocket.sh
+++ train_mini_rocket.sh
@@ -1,6 +1,6 @@
 python3 code/main.py train \
   --input-dir ../dataset \
   --model minirocket \
-  --model-kwargs num_kernels=1000 max_iter=30000 class_weight=balanced \
+  --model-kwargs num_kernels=1000 solver=saga max_iter=3000 tol=1e-4 \
   --sequence-length 100 \
   --stride 50
