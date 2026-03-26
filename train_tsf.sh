95f03845cb59fe99cbeb52892950538306c86ad3
Aaron Chan <amc9897@rit.edu>
2026-03-26 11:40:45 -0400

    REmove class weight balanced

--- train_tsf.sh
+++ train_tsf.sh
@@ -1,7 +1,7 @@
 python3 code/main.py train \
   --input-dir ../dataset \
   --model tsf \
-  --model-kwargs class_weight=balanced max_iter=2000 \
+  --model-kwargs max_iter=2000 \
   --sequence-length 100 \
   --stride 50 \
   --curve-fractions 0.1 0.2
