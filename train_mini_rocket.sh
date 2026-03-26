9d0d08e60a8cf0f50692d4da66088ce74eac32b9
Aaron Chan <amc9897@rit.edu>
2026-03-26 00:56:23 -0400

    Forgot to commit minirocket v3 which had ~13k iters

--- train_mini_rocket.sh
+++ train_mini_rocket.sh
@@ -1,6 +1,6 @@
 python3 code/main.py train \
   --input-dir ../dataset \
   --model minirocket \
-  --model-kwargs num_kernels=1000 max_iter=10000 class_weight=balanced \
+  --model-kwargs num_kernels=1000 max_iter=30000 class_weight=balanced \
   --sequence-length 100 \
   --stride 50
